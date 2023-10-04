61fc9cf29d6a653ab23f140e282828be386f8ccf
AbhishekRG96 <abhishekghogare45@gmail.com>
2023-10-04 13:29:09 +0530

    Inheritance practice

--- Basics of dart/inheritance/Program1.dart
+++ Basics of dart/inheritance/Program1.dart
@@ -2,32 +2,44 @@ class Parent{
 
   int x = 10;
   String str = "parent";
+  double d =10.5;
 
   void printData(){
     print(x);
     print(str);
+    print(d);
   }
 }
 
 class Child extends Parent{
 
   int x = 20;
-  String str2 = "child";
-
+  String str1 = "child";
+  double d2 = 20.5;
   void showData(){
     print(x);
-    print(str2);
+    print(str1);
+    print(d2);
   }
 }
 
 void main(){
   Child obj = new Child();
   
-  print(obj.x);
-  print(obj.str);
-  print(obj.x);
-  print(obj.str2);
-  obj.printData();
+  obj.printData();  
   obj.showData();
-
 }
+
+// Except constructor Everything gets inherited in child from parent
+
+/*
+OUTPUT : 
+
+20
+parent
+10.5
+20
+child
+20.5
+
+*/
